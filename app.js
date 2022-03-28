e7e083e12b71afdb22d7ee206bb434d3b6ee2338
Prabu <prabu.mgks@gmail.com>
2022-03-28 19:49:35 +0530

    app logic done

---
+++ app.js
@@ -0,0 +1,20 @@
+async function getData() {
+  try {
+    const res = await fetch("https://api.adviceslip.com/advice", {
+      method: "GET",
+    });
+    if (res.status < 400) {
+      return await res.json();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const adviceNo = document.querySelector("#advice-no");
+const advice = document.querySelector("#advice");
+
+getData().then((data) => {
+  adviceNo.textContent = "advice #" + data["slip"]["id"];
+  advice.textContent = `"${data["slip"]["advice"]}"`;
+});
